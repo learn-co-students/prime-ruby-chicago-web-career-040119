b720f92836e8843a13f31da3f7667f3d81a1994a
NMoore42 <nicholas.moore5@icloud.com>
2019-02-19 16:37:22 -0600

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(int)
   if int <= 1
-    return false
+    false
   end
   dividend_count = 2
   while dividend_count < int do
     if int % dividend_count == 0
-      return false
+      false
     end
     dividend_count += 1
   end
