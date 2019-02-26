0679433b7b0a020f1396873396fd309efccca5b4
Robert Tate <btate712@gmail.com>
2019-02-26 16:27:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(input)
+  if input < 2
+    return false
+  end
+  for attempt in 2..(input / 2)
+    if input % attempt == 0
+      return false
+    end
+  end
+  true
+end
